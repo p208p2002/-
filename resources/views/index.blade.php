19605b04b82f3850d0cbcb7fb5f0a3fc467dbaf6
p208p2002 <p208p2002@gmail.com>
2017-09-15 13:50:18 +0800

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="./login">使用者登入</a>
+    <a href="./logout">登出</a>
+    <a href="./register">註冊</a>
+    <a href="./admin/index">後臺管理</a>   
+</body>
+</html>
