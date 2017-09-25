3a87f9a73aaa0cdd1b6d38db654086873077c647
p208p2002 <p208p2002@gmail.com>
2017-09-25 09:29:18 +0800

    按鈕顏色

--- resources/views/admin/goodArticle.blade.php
+++ resources/views/admin/goodArticle.blade.php
@@ -14,6 +14,6 @@
 {{--  <a class="btn btn-warning" href="#" role="button">編輯消息</a>  --}}
 <a class="btn btn-danger" href={{ url('webAdmin/goodArticle/delete') }} role="button">刪除文章</a>
 <br><br>
-<a class="btn btn-danger" href={{ url('webAdmin/goodArticle/managerFilter') }} role="button">管理分類</a>
+<a class="btn btn-info" href={{ url('webAdmin/goodArticle/managerFilter') }} role="button">管理分類</a>
 {{--  <a class="btn btn-info" href={{ url('/webAdmin/newNews/managerFilter') }} role="button">管理分類</a>  --}}
 @endsection
