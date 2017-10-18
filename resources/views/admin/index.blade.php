8017ca6638889480e1df6807ae2c74fcae409629
p208p2002 <p208p2002@gmail.com>
2017-10-18 15:20:58 +0800

    status

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,4 +4,13 @@
     <h3>後臺首頁</h3>
     <br>
     <iframe src={{ url('/status.php') }} width="300" height="240" frameborder="0"></iframe>
+    <h3>線上課程功能已經上線!</h3>
+    <ul>待完成功能
+        <li>關於我們</li>
+        <li>活動紀實</li>
+        <li>分類功能(好文閱讀、線上課程)</li>
+        <li>RWD頁面優化</li>
+        <li>其他頁面優化</li>
+        <li>Dashbord選單點選顯示</li>
+    </ul>
 @endsection
