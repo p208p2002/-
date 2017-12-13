4f8e834ac68e4615990b9ceca6a673c24aaf739c
p208p2002 <p208p2002@gmail.com>
2017-12-13 15:59:03 +0800

    modify about-us

--- resources/views/website/aboutUs.blade.php
+++ resources/views/website/aboutUs.blade.php
@@ -9,9 +9,9 @@
         <img src="http://aeit.nutc.edu.tw/images/teacher/th4.jpg" width="150" alt="" srcset="">
     </div>
     <div class="col">
-        <h3 class="in-line">曾琦芬</h3> 教授
+        <h3 class="in-line">曾琦芬</h3> 副教授
         <hr>
-        英文姓名 : Tseng, Chi-Fen<br>
+        英文姓名 : Christine Chifen Tseng<br>
         最高學歷 : 國立臺灣師範大學英系博士<br>      
         學術專長 : 課程規劃與評鑑、英文寫作、教師信念、英語語音<br>
     </div>
@@ -23,9 +23,9 @@
         <img src="http://aeit.nutc.edu.tw/images/teacher/th3.jpg" width="150" alt="" srcset="">
     </div>
     <div class="col">
-        <h3 class="in-line">黃麗蓉</h3> 教授
+        <h3 class="in-line">黃麗蓉</h3> 助理教授
         <hr>
-        英文姓名 : Huang, Li-Jung <br>
+        英文姓名 : Li-Jung Huang<br>
         最高學歷 : 澳洲墨爾本大學語言學與應用語言學系博士<br>
         學術專長 : 語用學及英語教學<br>
     </div>
@@ -39,7 +39,7 @@
     <div class="col">
         <h3 class="in-line">陳媛珊</h3> 教授
         <hr>
-        英文姓名 : Chen, Yuan-Shan <br>        
+        英文姓名 : Yuanshan Chen <br>        
         最高學歷 : 國立台灣師範大學英語系博士 <br>
         學術專長 : 語用學及英語教學 <br>
     </div>
@@ -51,9 +51,9 @@
         <img src="http://aeit.nutc.edu.tw/images/teacher/th2.jpg" width="150" alt="" srcset="">
     </div>
     <div class="col">
-        <h3 class="in-line">吳雅玲</h3> 教授
+        <h3 class="in-line">吳雅玲</h3> 副教授
         <hr>
-        英文姓名 : Wu, Y-L <br>
+        英文姓名 : Ya-Ling Wu <br>
         最高學歷 : University Of Mississippi Curriculum & Instruction PhD With Emphasis In TESOL New York University Administration, Leadership And Technology <br>
         學術專長 : ESP專業英文(商務、管理)、中英口譯、TESOL <br>
     </div>
