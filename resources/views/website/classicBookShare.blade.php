0bda502a642bd90065a863802f38f8046ded532c
p208p2002 <p208p2002@gmail.com>
2018-01-17 15:12:08 +0800

    add href for block

--- resources/views/website/classicBookShare.blade.php
+++ resources/views/website/classicBookShare.blade.php
@@ -5,7 +5,9 @@
 @foreach($datas as $data)
     <div class="col-4">
         <div class="card" style="width: 20rem;">
-        <img class="card-img-top" src={{ url('/').$data->filepath }} alt="Card image cap">
+        <a href={{ url('/classicBookShare').'/'.$data->id}}>
+            <img class="card-img-top" src={{ url('/').$data->filepath }} alt="Card image cap">
+        </a>
         <div class="card-body">
         <h4 class="card-title text-center"><a style="color:black;" href={{ url('/classicBookShare').'/'.$data->id}}> {{ $data->title }} </a></h4>
         </div>
