c07f0b74c270e7e73408083c8eaeb68c0dcd03b3
unknown <p208p2002@gmail.com>
2019-03-11 21:21:02 +0800

    no message

--- resources/views/website/SpeechActivities.blade.php
+++ resources/views/website/SpeechActivities.blade.php
@@ -5,21 +5,19 @@
 @foreach($datas as $key=>$data)
     <div class="col-lg-4 col-12">
         <div class="card" style="width: 20rem;margin-bottom:20px;">
-        @if($data->imgfile!="empty")
-        <img class="card-img-top" src={{ url('/').$data->imgfile }} alt="Card image cap">
-        @else
-        <img class="card-img-top" src="https://image.flaticon.com/icons/svg/512/512500.svg" alt="Card image cap">
-        @endif
+            @if($data->imgfile!="empty")
+            <img class="card-img-top" src={{ url('/').$data->imgfile }} alt="Card image cap">
+            @else
+            <img class="card-img-top" src="https://image.flaticon.com/icons/svg/512/512500.svg" alt="Card image cap">
+            @endif
 
-        <div class="card-body">
-        <h4 class="card-title text-center"><a style="color:black;" href={{ url('/SpeechActivities').'/'.$data->id}}> {{ $data->eventname }} </a></h4>
-        <p class="text-secondary text-center">活動日期:{{$data->eventdate}}</p>
-        </div>
+            <div class="card-body">
+                <h4 class="card-title text-center"><a style="color:black;" href={{ url('/SpeechActivities').'/'.$data->id}}> {{ $data->eventname }} </a></h4>
+                <p class="text-secondary text-center">活動日期:{{$data->eventdate}}</p>
+            </div>
         </div>
     </div>
 @endforeach
 </div>
-
 {!! $datas->render() !!}
-
 @endsection
