37de2d81e57691a397eda90fb01f37caf6d508de
unknown <p208p2002@gmail.com>
2019-03-11 20:26:16 +0800

    rwd

--- resources/views/website/videoTrip.blade.php
+++ resources/views/website/videoTrip.blade.php
@@ -2,61 +2,20 @@
 @section('title', 'example')
 @section('container')
    {{--  code here  --}}
-
-@foreach($datas as $key=>$data)
-
-@if($key%3==0)
-<br>
 <div class="row">
-    <div class="col-4">
-        <div class="card" style="width: 20rem;">
-            <a href={{ url('/videoTrip/'.$data->id) }}>
-            <img class="card-img-top" src="https://image.flaticon.com/icons/svg/122/122628.svg" height="200" width="200" alt="Card image cap">
-            <div class="card-body">
-                <h4 class="card-title text-center">{{ $data->title }}</h4>
-            </a>
-                <p class="card-text text-secondary text-center">{{ $data->id }} | {{ $data->createtime }}</p>
-            </div>
-          
-        </div>
-    </div>
-@endif
-
-@if($key%3==1)
-    <div class="col-4">
-        <div class="card" style="width: 20rem;">
-            <a href={{ url('/videoTrip/'.$data->id) }}>
-            <img class="card-img-top" src="https://image.flaticon.com/icons/svg/122/122628.svg" height="200" width="200" alt="Card image cap">
-            <div class="card-body">
-                <h4 class="card-title text-center">{{ $data->title }}</h4>
-            </a>
-                <p class="card-text text-secondary text-center">{{ $data->id }} | {{ $data->createtime }}</p>
+    @foreach($datas as $key=>$data)
+        <div class="col-lg-4 col-12" style="margin-bottom:20px;">
+            <div class="card" style="width: 20rem;padding:20px;">
+                <a href={{ url('/videoTrip/'.$data->id) }}>
+                <img class="card-img-top" src="https://image.flaticon.com/icons/svg/122/122628.svg" height="200" width="200" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title text-center">{{ $data->title }}</h4>
+                </a>
+                    <p class="card-text text-secondary text-center">{{ $data->id }} | {{ $data->createtime }}</p>
+                </div>
             </div>
         </div>
-    </div>
-@endif
-
-@if($key%3==2)
-    <div class="col-4">
-        <div class="card" style="width: 20rem;">
-            <a href={{ url('/videoTrip/'.$data->id) }}>
-            <img class="card-img-top" src="https://image.flaticon.com/icons/svg/122/122628.svg" height="200" width="200" alt="Card image cap">
-            <div class="card-body">
-                <h4 class="card-title text-center">{{ $data->title }}</h4>
-            </a>
-                <p class="card-text text-secondary text-center">{{ $data->id }} | {{ $data->createtime }}</p>
-            </div>
-        </div>
-    </div>
-
+    @endforeach
 </div>
-@endif
 
-
-@endforeach
-
-@if(count($datas)%3 !=0)
-    </div>
-@endif
- 
 @endsection
