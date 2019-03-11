5feee57b828c2269048330abea48007668df268d
unknown <p208p2002@gmail.com>
2019-03-11 21:34:08 +0800

    rwd

--- resources/views/website/classicBookShare.blade.php
+++ resources/views/website/classicBookShare.blade.php
@@ -6,7 +6,9 @@
     <div class="col-lg-4 col-12">
         <div class="card" style="width: 20rem;">
         <a href={{ url('/classicBookShare').'/'.$data->id}}>
+            <div class="img-responsive">
             <img class="card-img-top" src={{ url('/').$data->filepath }} alt="Card image cap">
+            </div>
         </a>
         <div class="card-body">
         <h4 class="card-title text-center"><a style="color:black;" href={{ url('/classicBookShare').'/'.$data->id}}> {{ $data->title }} </a></h4>
