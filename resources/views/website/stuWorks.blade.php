e07093c41e7c724a2075ed84c375c114f81e78e5
unknown <p208p2002@gmail.com>
2019-03-11 20:32:07 +0800

    css style

--- resources/views/website/stuWorks.blade.php
+++ resources/views/website/stuWorks.blade.php
@@ -3,16 +3,16 @@
 @section('container')
 
 <div class="row">
-    @foreach($datas as $data)    
-    
-    <div class="col-12 col-md-4 col-lg-3">
+    @foreach($datas as $data)
+
+    <div class="col-12 col-md-4 col-lg-4">
         <a href={{ url('/stu-performance/school' ).'/'.$data->schoolid.'/'.$data->id }} style="color:black; text-decoration:none;">
         <div class="card">
             <div class="card-body">
             @if($data->imgpath!="")
-            <img src={{ url($data->imgpath) }} alt="" srcset="" class="card-img-top">
+            <img src={{ url($data->imgpath) }} alt="" srcset="" class="card-img-top" style="height:250px;">
             @else
-            <img src="https://image.flaticon.com/icons/png/512/554/554826.png" alt="" srcset="" class="card-img-top">
+            <img src="https://image.flaticon.com/icons/png/512/554/554826.png" alt="" srcset="" class="card-img-top" style="height:250px;">
             @endif
               <p class="card-text text-center">{{ $data->className }}</p>
             </div>
