a13f7867cc448e0c4a481ebc39b881e0c8823c1c
unknown <p208p2002@gmail.com>
2019-03-11 21:15:47 +0800

    no message

--- resources/views/website/SpeechActivities.blade.php
+++ resources/views/website/SpeechActivities.blade.php
@@ -1,32 +1,24 @@
 @extends('website.layouts.main')
 @section('title', 'example')
 @section('container')
-@foreach($datas as $key=>$data)
 <div class="row">
-        <div class="col-lg-4 col-12">
-            <div class="card" style="width: 20rem;margin-bottom:20px;">
-            @if($data->imgfile!="empty")
-            <img class="card-img-top" src={{ url('/').$data->imgfile }} alt="Card image cap">
-            @else
-            <img class="card-img-top" src="https://image.flaticon.com/icons/svg/512/512500.svg" alt="Card image cap">
-            @endif
+@foreach($datas as $key=>$data)
+    <div class="col-lg-4 col-12">
+        <div class="card" style="width: 20rem;margin-bottom:20px;">
+        @if($data->imgfile!="empty")
+        <img class="card-img-top" src={{ url('/').$data->imgfile }} alt="Card image cap">
+        @else
+        <img class="card-img-top" src="https://image.flaticon.com/icons/svg/512/512500.svg" alt="Card image cap">
+        @endif
 
-            <div class="card-body">
-            <h4 class="card-title text-center"><a style="color:black;" href={{ url('/SpeechActivities').'/'.$data->id}}> {{ $data->eventname }} </a></h4>
-            <p class="text-secondary text-center">活動日期:{{$data->eventdate}}</p>
-            </div>
-            </div>
+        <div class="card-body">
+        <h4 class="card-title text-center"><a style="color:black;" href={{ url('/SpeechActivities').'/'.$data->id}}> {{ $data->eventname }} </a></h4>
+        <p class="text-secondary text-center">活動日期:{{$data->eventdate}}</p>
+        </div>
         </div>
     </div>
 @endforeach
-
-<!--  -->
-@if(sizeof($datas)%3 == 2)
-<div class="col"></div>
-@endif
-@if(sizeof($datas)%3 != 0)
 </div>
-@endif
 
 {!! $datas->render() !!}
 
